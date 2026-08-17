38b94e054ea928224d25af5482677cb2e176d77d
Akansha Gupta <akanshaa649@gmail.com>
2026-08-17 23:19:55 +0530

    Time: 8 ms (54.29%) | Memory: 22.9 MB (87.45%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool canEat(vector<int>& piles, int h, int speed) {
+    long long hours = 0;
+
+    for (int pile : piles) {
+        hours += (pile + speed - 1) / speed;
+
+        if (hours > h)
+            return false;
+    }
+
+    return true;
+}
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low = 1;
+    int high = *max_element(piles.begin(), piles.end());
+
+    while (low < high) {
+        int mid = low + (high - low) / 2;
+
+        if (canEat(piles, h, mid))
+            high = mid;
+        else
+            low = mid + 1;
+    }
+
+    return low;
+    }
+};
